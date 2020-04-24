df969132725a4cc9945e274f9f1d36e23e6bcd55
Mudit Sharma <muditsharma23702@gmail.com>
2020-04-24 15:16:41 +0530

    Add files via upload

---
+++ 43.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int rows, k=0;
+    printf("Enter number of rows:\n ");
+    scanf("%d",&rows);
+    for(int i=1; i<=rows; ++i, k=0)
+    {
+        for(int space=1; space<=rows-i; ++space)
+        {
+            printf("  ");
+        }
+        while(k != 2*i-1)
+        {
+            printf("* ");
+            ++k;
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
