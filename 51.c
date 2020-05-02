0872a29c1e7a80f731e8f4f023f8dd54a53e9847
Mudit Sharma <muditsharma23702@gmail.com>
2020-05-02 12:53:16 +0530

    Add files via upload

---
+++ 51.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char s1[]="mudit";
+    char s2[]="sharma";
+    printf("the length of s1 and s2 are:%d %d\n",strlen(s1),strlen(s2));
+    return 0;
+}
