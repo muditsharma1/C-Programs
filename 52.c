95f7a7dc52584c9983ec08f202939168ab2acec1
Mudit Sharma <muditsharma23702@gmail.com>
2020-05-02 12:53:35 +0530

    Add files via upload

---
+++ 52.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char s1[] = "mudit";
+    char s2[] = "sharma";
+    printf("the reversed string s1 is:");
+    puts(strrev(s1));
+    printf("the reversed string s2 is:");
+    puts(strrev(s2));
+    return 0;
+}
